90a6c0e21895a07b81f6184fcb5dbacc6fa2a313
ajsheehan1 <alexander.j.sheehan@gmail.com>
2020-07-23 11:11:12 -0400

    second commit

--- ElevenNote/ElevenNote.Data/Note.cs
+++ ElevenNote/ElevenNote.Data/Note.cs
@@ -7,17 +7,17 @@ using System.Threading.Tasks;
 
 namespace ElevenNote.Data
 {
-    class Note
+    public class Note
     {
         [Key]
         public int NoteId { get; set; }
         [Required]
         public Guid OwnerId { get; set; }
         [Required]
-        [Display(Name = "Your Note")]
         public string Title { get; set; }
         [Required]
         public string Content { get; set; }
+        
         [Required]
         public DateTimeOffset CreatedUtc { get; set; }
 
